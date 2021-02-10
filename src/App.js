77f61259969b7ada3c612c2d7ca33e781673bdca
Alex Schmitt <alex.schmitt@outlook.com>
2021-02-10 20:44:02 -0300

    Conclusão exercício 9

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GitHubUser from "./components/Card";
 import { makeStyles } from "@material-ui/core/styles";
+import { grey } from "@material-ui/core/colors";
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   button: {
     margin: 15,
   },
+  input: {
+    color: grey,
+  },
 });
 function App() {
-  const API = "https://api.github.com/users/ximitti";
+  const API_DEFAULT = "https://api.github.com/users/ximitti";
   const classes = useStyles();
+  const [api, setApi] = useState("https://api.github.com/users/ximitti");
   const [user, setUser] = useState({});
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState(true);
+  const [search, setSearch] = useState("");
 
-  const handleToggle = () => {
-    fetch(API)
+  useEffect(() => {
+    search ? getData(api) : getData(API_DEFAULT);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [api]);
+
+  const getData = (link) => {
+    fetch(link)
       .then((response) => response.json())
       .then((user) => setUser(user))
       .catch((err) => console.log(err));
+  };
 
+  const handleToggle = () => {
     setActive(!active);
+  };
+
+  const submitUser = (event) => {
+    event.preventDefault();
+    const newApi = `https://api.github.com/users/${search}`;
+    setApi(newApi);
   };
 
   return (
     <div className="App">
       <header className="App-header">
+        <form default noValidate autoComplete="off" onSubmit={submitUser}>
+          <TextField
+            className={classes.input}
+            id="outlined-search"
+            label="Pesquisar GitHub User"
+            type="search"
+            variant="outlined"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <Button
+            className={classes.button}
+            size="medium"
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Pesquisar
+          </Button>
+        </form>
         <Button
           className={classes.button}
           size="large"
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 200,
+    minWidth: 200,
   },
   media: {
-    height: 140,
+    height: 200,
   },
 });
 
@@ -19,24 +19,22 @@ const GitHubUser = (props) => {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={props.user.avatar_url}
-          title={props.user.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.user.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Following: {props.user.following}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Public Repos: {props.user.public_repos}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image={props.user.avatar_url}
+        title={props.user.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {props.user.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Following: {props.user.following}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Public Repos: {props.user.public_repos}
+        </Typography>
+      </CardContent>
     </Card>
   );
 };
